b11d4357b86bc54dbbdc555d8579cb9d98b895f0
sarahlte <sarah_leconte@msn.com>
2024-01-19 13:58:06 +0100

    qq

--- views/layout.php
+++ views/layout.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Soucishop</title>
-    <link rel="stylesheet" href="./styles.css">
+    <link rel="stylesheet" href="./styles/style.css">
 </head>
-<body>
+<body id="body">
     <header>
 
     </header>
