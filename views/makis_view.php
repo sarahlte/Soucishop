eea72348545209a9b53a1c6e4c19c017f0aa2182
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-01-22 14:01:34 +0100

    add
    images + mise en page sushis et makis

--- views/makis_view.php
+++ views/makis_view.php
@@ -3,9 +3,19 @@ require './controllers/products_controller.php';
 ?>
 
 <div>
-    <ul>
+    <ul><div class="container hp-card-contain">
         <?php foreach ($products as $product) : ?>
-            <li><?= $product['nom'] ?>, <?= $product['prix vente'] ?>, <?= $product['description'] ?> <img src="./assets/<?= $product['img1']?>" alt=""></li>
+            <div class="hp-card">
+                <div class="hp-img"><img src="./assets/<?= $product['img1']?>" class="hp-button-img"></div>
+                <div class="hp-txt-card">
+                    <div class="card-title"><?= $product['nom'] ?></div>
+                    <div class="card-txt"><?= $product['description'] ?></div>
+                    <div class="card-price">
+                        <div href="?page=sushis" class="card-link-price"><?= $product['prix vente'] ?> €</div>
+                        <a href="?page=sushis" class="card-link-price">ajouter au panier -></a>
+                    </div>
+                </div>
+            </div>
         <?php endforeach; ?>
-    </ul>
+    </div>
 </div>
