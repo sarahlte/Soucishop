ec68f717043d8bd081e4bd592a49ce034cda0d0c
sarahlte <sarah_leconte@msn.com>
2024-01-22 12:28:37 +0100

    add

--- views/sushis_view.php
+++ views/sushis_view.php
@@ -5,7 +5,7 @@ require './controllers/products_controller.php';
 <div>
     <ul>
         <?php foreach ($products as $product) : ?>
-            <li><?= $product['nom'] ?>, <?= $product['prix vente'] ?>, <?= $product['description'] ?> <img src="<?= $product['img1']?>" alt=""></li>
+            <li><?= $product['nom'] ?>, <?= $product['prix vente'] ?>, <?= $product['description'] ?> <img src="./assets/<?= $product['img1']?>" alt=""></li>
         <?php endforeach; ?>
     </ul>
 </div>
