9e88784de85749df3446a6834e08029462e7474f
maelanglois <150166864+maelanglois@users.noreply.github.com>
2024-01-23 16:27:58 +0100

    add

--- controllers/profil_controller.php
+++ controllers/profil_controller.php
@@ -27,3 +27,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'password' => $password,
     ]);
 }
+
+if ($updateStatement->execute()) {
+    $_SESSION['status'] = 'success';
+    $_SESSION['message'] = 'Votre profil a bien été modifié.';
+    header("Location: ?page=connexion");
+    exit();
+}
