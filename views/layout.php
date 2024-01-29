a92072c1acc9a6c9184f41ca0b26ef92b4c2d92e
sarahlte <sarah_leconte@msn.com>
2024-01-29 11:19:14 +0100

    add

--- views/layout.php
+++ views/layout.php
@@ -29,8 +29,8 @@
             <li><a class="menu__item" href="?page=profil">Mon profil</a></li>
             <?php if(isset($_SESSION['role']) && $_SESSION['role'] == 'admin'):?>
             <li><a class="menu__item" href="?page=add">Ajouter un produit</a></li>
-            <li><a class="menu__item" href="?page=add">Ajouter un utilisateur</a></li>
-            <li><a class="menu__item" href="?page=add_view">Gestion des bons de réduction</a></li>
+            <li><a class="menu__item" href="?page=add-admin">Ajouter un utilisateur</a></li>
+            <li><a class="menu__item" href="?page=add">Gestion des bons de réduction</a></li>
             <li><a class="menu__item" href="?page=order">Commandes</a></li>
             <li><a class="menu__item" href="#">Chiffre d'affaire</a></li>
             <?php endif ?>
